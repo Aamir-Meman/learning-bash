a8d6973bb6746b52c454410f85dad3e7e0a3669f
Aamir M Meman <aamirmemon2292@gmail.com>
2022-07-28 22:09:00 +0000

    added new folder and example

---
+++ les03/les03_instructor_examples/firstscript.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+clear
+echo "SCRIPT BEGINS"
+
+echo "Hello $LOGNAME"
+echo
+
+echo "Todays date and time is: "$(date)
+echo
+
+mynum=108
+myday="Thursday"
+
+echo "The value of my num is $mynum"
+echo "The value of my day is $myday"
+echo
+
+echo "SCRIPT FINISHED!!"
+echo
+
+
+
+
