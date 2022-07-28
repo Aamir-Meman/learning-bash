72701b99be7b39e96183aaa0f5da239a4f6e07a3
Aamir M Meman <aamirmemon2292@gmail.com>
2022-07-28 02:09:03 +0000

    first commit

---
+++ les03/echoscript1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+clear
+echo "SCRIPT BEGINS"
+
+echo "Hello $LOGNAME"
+
+echo "Todays date is:" $(date '+%m/%d/%y')
+
+echo "and the current time is:" $(date '+%H:%M:%S%n')
+
+#date '+%H:%M:%S%n'
+
+echo "Now a list of the processes on the current shell"
+ps
+
+echo "SCRIPT FINISHED!!"
