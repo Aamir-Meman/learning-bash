3b9ee7250963fe1f196df7576e69eecea1829da3
Aamir M Meman <aamirmemon2292@gmail.com>
2022-08-17 18:57:25 +0000

    adding the more examples in les04

---
+++ les04/mathlet.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+let arg1=12
+let arg2=11
+
+let add=$arg1+$arg2
+let sub=$arg1-$arg2
+let mul=$arg1*$arg2
+let div=$arg1/$arg2
+let rmdr=$arg1%$arg2
+echo $add $sub $mul $div $rmdr
+
+
+
+
+
+
+
