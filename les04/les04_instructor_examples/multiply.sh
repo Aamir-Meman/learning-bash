44ece48db6b631b27554a94312c18d4ed24489ef
Aamir M Meman <aamirmemon2292@gmail.com>
2022-08-19 23:38:40 +0000

    added multiply sh file

---
+++ les04/les04_instructor_examples/multiply.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script Name: multiply.sh
+# Purpose: To take a value from the parameter list and multiple it by itself.
+#
+
+num=8
+
+echo "The number is: $num"
+(( result = num * num ))
+echo "The number multiplied by itself is: $result"
+
+
+
+
+
