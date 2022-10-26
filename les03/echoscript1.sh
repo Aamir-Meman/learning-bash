fde00066c208885072961c87411a11916a25e430
Aamir M Meman <aamir.m.meman@oracle.com>
2022-10-26 14:33:36 -0400

    update the script

--- les03/echoscript1.sh
+++ les03/echoscript1.sh
@@ -5,9 +5,9 @@ echo "SCRIPT BEGINS"
 
 echo "Hello $LOGNAME"
 
-echo "Todays date is:" $(date '+%m/%d/%y')
+echo "Todays date is: $(date '+%m/%d/%y')"
 
-echo "and the current time is:" $(date '+%H:%M:%S%n')
+echo "and the current time is: $(date '+%H:%M:%S%n')"
 
 echo "Now a list of the processes on the current shell"
 ps
