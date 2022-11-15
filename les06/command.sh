4b94146a6b1c52301e0f73ae9b1e12a97a12ba0d
Aamir M Meman <aamir.m.meman@oracle.com>
2022-11-14 21:27:53 -0500

    added the command for deleting the text

--- les06/command.sh
+++ les06/command.sh
@@ -12,4 +12,9 @@ sed 's/3/X/g' data.file #this will substitute the first occurence of 3 with X gl
 sed -n '/ [0-9]$/p' data.file #this will print out the pattern[/] which ended in the single digit.
 sed 's/ [0-9]$/& Single Digit/' data.file # this will substitue the ending with single digit to some text. &[ambersand] will hold the old string value and then it will replace with Single Digit.
 sed '/East/r northmesg' data.file # this will read the pattern East in the data.file and then will insert the northmesg.
+### Deleting Text
+sed '4,8d' data.file # Don't print from the range 4 to 8 lines
+sed '/East/d' data.file # Don't display the pattern starts with East in file.
+sed '/^West/d' data.file # Don't display the pattern starts with W
+sed '/Central/,/South/d' data.file # this command will not display the range from Central to the South.
 
