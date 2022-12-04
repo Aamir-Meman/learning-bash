d4b757c6d719d2c035a21f0c147667352b4d16f2
Aamir M Meman <aamir.m.meman@oracle.com>
2022-12-04 17:00:22 -0500

    added few commands for running nawk script

--- les07/command.sh
+++ les07/command.sh
@@ -16,3 +16,15 @@ nawk 'BEGIN { print "Eastern Regions\n" }; /East/ { print $5, $4 } END { print "
 # nawk scripts
 nawk -f report.nawk data.file
 nawk -f report2.nawk data.file
+# Input Field Seperator (FS)
+nawk -F: ' { print $1, $2, $3, $4 } ' /etc/group
+nawk -F: ' { print $1 "\t" $4 } ' /etc/group
+nawk ' BEGIN { FS=":" }; { print $1, $4 }' /etc/group
+nawk ' BEGIN { FS=":" }; { print $1, $3 }' /etc/group
+nawk -f report3.nawk /etc/group
+# Output Field Seperator
+nawk ' { print $1 $2 $3 } ' data.file
+nawk ' { print $1, $2, $3 } ' data.file
+nawk ' { print $1 "\t" $2, $3 } ' data.file
+nawk 'BEGIN { OFS="\t" }; { print $1, $2, $3 }' data.file
+nawk 'BEGIN { OFS="\t" }; { print $3, $4, $2 }' data.file
