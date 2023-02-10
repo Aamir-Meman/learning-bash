26f86e9a3ef17e28c54993a3baf51a15547a3ab1
Aamir M Meman <aamir.m.meman@oracle.com>
2023-02-10 12:43:12 -0500

    added few commands of chapter 07

--- les07/les07_instructor_examples/command.sh
+++ les07/les07_instructor_examples/command.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
  # Print the first name and last name from the data.file
 nawk ' { print "Name : " , $3 , $2, "\t", "Region : ", $1 } ' data.file
+nawk ' /[A-Z]E/ { print $2, $3, $4, $1 } /E[A-Z]/ { print $2, $3, $4, $1 } ' data.file # followed or preceded with E
+nawk ' BEGIN { print "Developers in Eastern regions\n" } ; /[A-Z]e/ { print $2, $3, $4, $1 } /E[A-Z]/ { print $2, $3, $4, $1 } ' data.file # Use of BEGIN
 
