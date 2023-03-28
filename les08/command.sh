e461759def815d20213966be77dd0f4d28c5ce4b
Aamir M Meman <aamir.m.meman@oracle.com>
2023-03-28 18:07:43 -0400

    fd, here and i/o

--- les08/command.sh
+++ les08/command.sh
@@ -5,26 +5,26 @@ printf "\aWrite your details: \n Name: " # Rings the bells on the terminal, whic
 printf "%5d%4d\n" 7 89 789 6789 56789 # this will display the result with 5 space and 4 space
 
 # The echo Statement
-echo "Hello There \nHow are you?" # In bash this command will not work for new line
+echo -e "Hello There \nHow are you?" # In bash this command will not work for new line
 echo "-2 was the temperature this morning"
 echo -n "No newline printed here  " # To suppress the new line character use -n # In bash system
-echo "Hello\tout\tthere" # tab command is not working in bash
+echo -e "Hello\tout\tthere" # tab command is not working in bash
 
 # The read statement
-read var1 var2 var3
-read num string junk # More tokens then variables
+read -pr var1 var2 var3
+read -pr num string junk # More tokens then variables
 #134 bye93; alk the rest of the line is put in 'junk'
-read token1 token2 #<enter>
+read -pr token1 token2 #<enter>
 #One  # token2 will be NULL
 read 
 #What is this saved in?
-echo $REPLY
+echo -e $REPLY
 #What is this saved in?
 read #bornshell
 # read : missing arguments
 
 ls -l /etc/passwd > file
-read a b c d e f g h < file
+read -pr a b c d e f g h < file
 
 # output error logs
 ls -l foobar.txt 2> error.logs
