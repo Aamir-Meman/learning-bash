a3168fb14837c5be5769a12a2cd977e68735fa7d
Aamir M Meman <aamir.m.meman@oracle.com>
2023-03-29 17:55:58 -0400

    declare and typeset

---
+++ les09/strman1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script name: strman1.sh
+
+typeset -r word="happy"
+
+typeset -r word1="depressed"
+
+echo -e "123456789"
+echo $word
+echo 
+
+echo -e "123456789"
+echo $word1
---
+++ les09/strman2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script name: strman2.sh
+
+declare -r word="happy"
+
+declare -l word1="Depressed" # -l to make each word to lowercase
+
+echo -e "123456789"
+echo -e "$word"
+echo
+
+echo -e "123456789"
+echo -e "$word1"
+echo
