9c212a97f9c75f0bbef9510b59cf96640d1fecfd
Aamir M Meman <aamir.m.meman@oracle.com>
2023-04-03 12:48:34 -0400

    adding more commands for chpt09

--- les09/command.sh
+++ les09/command.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+# Removing portions of a string 
+stringx=/usr/bin/local/bin
+echo -e ${stringx%/bin}
+# /usr/bin/local # This will remove the smallest right most substring of string
+echo -e ${stringx%%/bin}
+# /usr/bin/local # This will remove the largest right-most substring of string
