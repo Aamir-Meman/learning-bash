207c2d78bd928861c61b8c407c2745d9412180a0
Aamir M Meman <aamir.m.meman@oracle.com>
2023-07-19 19:52:55 -0400

    Adding getops script

--- les11/getopts1.sh
+++ les11/getopts1.sh
@@ -2,4 +2,21 @@
 # Script name: getopts1.sh
 USAGE="usage: $0 [-d] [-m month]"
 year=$(date +%Y) # 2023
-
+while getopts :dm: opt_char; do
+    case $opt_char in
+    d)
+        echo -n "Date: " # -d option given date
+        date 
+        ;;
+    m)
+        cal "$OPTARG" "$year" # -m option given with an arg
+        ;;
+    \?)
+        echo "$OPTARG is not a valid option."
+        echo "$USAGE"
+        ;;
+    :)
+        echo "$OPTARG option requires an argument."
+        echo "$USAGE"
+    esac
+done
