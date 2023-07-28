7eb4c6d35837ff6ace233e01d52aad129404aab8
Aamir M Meman <aamir.m.meman@oracle.com>
2023-07-28 13:05:44 -0400

    changes

--- les08/demoheredoc.sh
+++ les08/demoheredoc.sh
@@ -4,4 +4,4 @@ read -p "Please provide the value for var1 : " var1
 read -p "Please provide the value for var2 : " var2
 
 echo -e "The value of var1 is: $var1"
-echo -e "The value of var2 is: $var2" 
+echo -e "The value of var2 is: $var2"
